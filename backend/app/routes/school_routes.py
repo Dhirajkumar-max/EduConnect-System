a3d4fdba659952114c88e898e7068de26534acca
Dhirajkumar-max <thakurdhiraj791@gmail.com>
2026-05-20 21:47:25 +0530

    CRUD APIs completed

--- backend/app/routes/school_routes.py
+++ backend/app/routes/school_routes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from bson import ObjectId
 from app.models.school_model import School
 from app.database.connection import database
 
@@ -14,4 +15,70 @@ async def add_school(school: School):
     return {
         "message": "School added successfully",
         "id": str(result.inserted_id)
+    }
+
+@router.get("/schools")
+async def get_schools():
+
+    schools = []
+
+    async for school in database.schools.find():
+
+        school["_id"] = str(school["_id"])
+
+        schools.append(school)
+
+    return {
+        "schools": schools
+    }
+
+@router.get("/school/{school_id}")
+async def get_school(school_id: str):
+
+    school = await database.schools.find_one(
+        {"_id": ObjectId(school_id)}
+    )
+
+    if school:
+
+        school["_id"] = str(school["_id"])
+
+        return school
+
+    return {
+        "message": "School not found"
+    }
+@router.put("/update-school/{school_id}")
+async def update_school(school_id: str, school: School):
+
+    updated_data = school.dict()
+
+    result = await database.schools.update_one(
+        {"_id": ObjectId(school_id)},
+        {"$set": updated_data}
+    )
+
+    if result.modified_count == 1:
+        return {
+            "message": "School updated successfully"
+        }
+
+    return {
+        "message": "School not found or no changes made"
+    }
+@router.delete("/delete-school/{school_id}")
+async def delete_school(school_id: str):
+
+    result = await database.schools.delete_one(
+        {"_id": ObjectId(school_id)}
+    )
+
+    if result.deleted_count == 1:
+
+        return {
+            "message": "School deleted successfully"
+        }
+
+    return {
+        "message": "School not found"
     }
